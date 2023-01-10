dbef5d85ada4813d57cbb7d6723da23e7db64d49
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-01-10 17:55:38 -0500

    feat: signed and expires cookies

--- app.js
+++ app.js
@@ -2,17 +2,18 @@ const express = require('express')
 const cookieParser = require('cookie-parser')
 const app = express()
 
-app.use(cookieParser())
+app.use(cookieParser('secret'))
 app.use(express.json())
 
 const server = app.listen(3000, () => console.log('Server Up!'))
 
 app.get('/set-cookie', (req, res) => {
-    res.cookie('coderhouse', 'Coder was here again!!').send({ message: "Cookie setted!" })
+    res.cookie('coderhouse', 'Coder was here again!!', { signed: true }).send({ message: "Cookie setted!" })
 })
 
 app.get('/get-cookie', (req, res) => {
-    res.send(req.cookies.coderhouse)
+    // res.send(req.cookies)
+    res.send(req.signedCookies)
 })
 
 app.get('/clear/:cookieName', (req, res) => {
