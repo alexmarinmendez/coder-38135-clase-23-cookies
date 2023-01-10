0268316a1a6e171fbed9b8e2825a56d8f3c9642a
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-01-10 18:10:16 -0500

    feat: desafio de clase

--- app.js
+++ app.js
@@ -2,21 +2,21 @@ const express = require('express')
 const cookieParser = require('cookie-parser')
 const app = express()
 
-app.use(cookieParser('secret'))
+app.use(cookieParser('c0d3r'))
 app.use(express.json())
 
 const server = app.listen(3000, () => console.log('Server Up!'))
 
-app.get('/set-cookie', (req, res) => {
-    res.cookie('coderhouse', 'Coder was here again!!', { signed: true }).send({ message: "Cookie setted!" })
+app.post('/cookies', (req, res) => {
+    let cookie = req.body
+    if (!cookie.name || !cookie.value) return res.send({ message: "Faltan valores" })
+    res.cookie(cookie.name, cookie.value).send({ message: "Galleta creada" })
 })
 
-app.get('/get-cookie', (req, res) => {
-    // res.send(req.cookies)
-    res.send(req.signedCookies)
+app.get('/cookies', (req, res) => {
+    res.send({ galletitas: req.cookies })
 })
 
-app.get('/clear/:cookieName', (req, res) => {
-    const {cookieName} = req.params
-    res.clearCookie(cookieName).send({ message: 'Cookie deleted!' })
+app.delete('/cookies/:cookieName', (req, res) => {
+    res.clearCookie(req.params.cookieName).send({ message: "Cookie deleted!" })
 })
